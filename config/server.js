1bc8dbcbef79342b6c3ac59c671b9aa4fbb0d05a
Farman <farman.pirz@gmail.com>
2020-10-17 14:13:57 -0600

    Initial project

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '47f0559a0023d648e1209a0122666947'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '26e223b8-fc5e-4b18-ad6f-ae7236f38c52'
+};
